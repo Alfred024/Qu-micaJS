134b05e4b780868370e864ca9ac4b8872cbcaa3b
Alfredo-JT <alfredo.jimeneztellez9@gmail.com>
2023-08-10 11:46:15 -0600

    Función para borrar el último elemento agregado

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const Compound = class{
     }
 }
 const compunds = [
-    {x: new Compound("Agua destilada","H ,H ,O ,","H2O")},
-    {x: new Compound("Cloruro de sodio","Na,Cl,","NaCl")},
-    {x: new Compound("Ácido Clorhídrico","H ,Cl,","HCl")},
-    {x: new Compound("Ácido sulfúrico","H ,H ,S ,O ,O ,O ,O ,","H2SO4")},
-    {x: new Compound("Ácido nítrico","H ,N ,O ,O ,O ,","HNO3")},
-    {x: new Compound("Álcohol Etílico","C ,H ,H ,H ,C ,H ,H ,O ,H ,","CH3CH2OH")},
-    {x: new Compound("Metano","C ,H ,H ,H ,H ,","CH4")},
-    {x: new Compound("Hidróxido de Sodio","Na,O ,H ,","NaOH")},
-    {x: new Compound("Hidróxido de Potasio","K ,O ,H ,","KOH")},
-    {x: new Compound("Gas propano","C ,C ,C ,H ,H ,H ,H ,H ,H ,H ,H ,","C3H8")},
-    {x: new Compound("Ácido cítrico","C ,C ,C ,C ,C ,C ,H ,H ,H ,H ,H ,H ,H ,H ,O ,O ,O ,O ,O ,O ,O ,","C6H8O7")},
-    {x: new Compound("Ácido fosfórico","H ,H ,H ,P ,O ,O ,O ,O ,","H3PO4")},
-    {x: new Compound("Ácido sulfuroso","H ,H ,S ,O ,O ,O ,","H2SO3")},
-    {x: new Compound("Hidróxido de Magnesio","Mg,O ,H ,O ,H ,","Mg(OH)2")},
-    {x: new Compound("Hidróxido de Calcio","Ca,O ,H ,O ,H ,","Ca(OH)2")},
+    {compound: new Compound("Agua destilada","H ,H ,O ,","H2O")},
+    {compound: new Compound("Cloruro de sodio","Na,Cl,","NaCl")},
+    {compound: new Compound("Ácido Clorhídrico","H ,Cl,","HCl")},
+    {compound: new Compound("Ácido sulfúrico","H ,H ,S ,O ,O ,O ,O ,","H2SO4")},
+    {compound: new Compound("Ácido nítrico","H ,N ,O ,O ,O ,","HNO3")},
+    {compound: new Compound("Álcohol Etílico","C ,H ,H ,H ,C ,H ,H ,O ,H ,","CH3CH2OH")},
+    {compound: new Compound("Metano","C ,H ,H ,H ,H ,","CH4")},
+    {compound: new Compound("Hidróxido de Sodio","Na,O ,H ,","NaOH")},
+    {compound: new Compound("Hidróxido de Potasio","K ,O ,H ,","KOH")},
+    {compound: new Compound("Gas propano","C ,C ,C ,H ,H ,H ,H ,H ,H ,H ,H ,","C3H8")},
+    {compound: new Compound("Ácido cítrico","C ,C ,C ,C ,C ,C ,H ,H ,H ,H ,H ,H ,H ,H ,O ,O ,O ,O ,O ,O ,O ,","C6H8O7")},
+    {compound: new Compound("Ácido fosfórico","H ,H ,H ,P ,O ,O ,O ,O ,","H3PO4")},
+    {compound: new Compound("Ácido sulfuroso","H ,H ,S ,O ,O ,O ,","H2SO3")},
+    {compound: new Compound("Hidróxido de Magnesio","Mg,O ,H ,O ,H ,","Mg(OH)2")},
+    {compound: new Compound("Hidróxido de Calcio","Ca,O ,H ,O ,H ,","Ca(OH)2")},
 ]
 
 const Element = class{
@@ -56,7 +56,7 @@ for (let i = 0; i < elementosNum; i++) {
             checkAnswer(index);
             createNewElement(this);
             if(started){
-                if(index === compunds[compuestoRandom].x.sequence.length+3){
+                if(index === compunds[compuestoRandom].compound.sequence.length+3){
                     compuestoRandomExplored.push(compuestoRandom);
                     sequence = "";
                     const answer = document.getElementById("viewSequence");
@@ -70,6 +70,10 @@ for (let i = 0; i < elementosNum; i++) {
                 const elements = document.querySelectorAll("#viewSequence button");
                 const lastElement = elements[lastElementIndex];
                 lastElement.setAttribute("style","background-color: red;")
+                //Aquí podemos eliminar el elemento incorrecto si así lo queremos
+                lastElement.addEventListener('click', function () {
+                    deleteLastElement(lastElement);
+                });
             }
         }else{
             //Despliega info del elemento seleccionado (Nombre, número atómico, etc)
@@ -79,6 +83,7 @@ for (let i = 0; i < elementosNum; i++) {
 }
 
 async function randomCompound(){
+    restart();
     //document.getElementById("restartBtn").classList.add("hidden");
     document.getElementById("tablaCompuestos").removeAttribute("hidden");
     sequence = "";
@@ -91,13 +96,13 @@ async function randomCompound(){
     const section = document.querySelector(".info");
     section.removeAttribute("hidden");
     const divCreated = document.getElementById("compuestoRandom");
-    divCreated.innerHTML ="Seleccione los elementos para formar el compuesto: "+compunds[compuestoRandom].x.name;
-    console.log("Secuencia solicitada: "+compunds[compuestoRandom].x.sequence);
+    divCreated.innerHTML ="Seleccione los elementos para formar el compuesto: "+compunds[compuestoRandom].compound.name;
+    console.log("Secuencia solicitada: "+compunds[compuestoRandom].compound.sequence);
     started = true;
 }
 
 async function checkAnswer(actualIndex){
-    let realSequenceAtIndex = compunds[compuestoRandom].x.sequence.substr(0, actualIndex);
+    let realSequenceAtIndex = compunds[compuestoRandom].compound.sequence.substring(0, actualIndex);
 
     if(realSequenceAtIndex === sequence){
         index += 3;
@@ -133,14 +138,20 @@ async function createNewElement(elementPressed){
     const newElement = document.createElement("button");
     newElement.innerHTML = elementPressed.innerHTML;
     answerLayout.appendChild(newElement);
+    //newElement.addEventListener('click', deleteLastElement());
+}
+async function deleteLastElement(lastElement){
+    sequence = sequence.substring(0, index-3);
+    lastElement.remove();
+    started = true;
 }
 async function creteNewTableRow(){
     const table = document.getElementById("tablaCompuestos");
     const newRow = document.createElement("tr");
     const nameCompuesto = document.createElement("th");
-    nameCompuesto.innerHTML = compunds[compuestoRandom].x.name;
+    nameCompuesto.innerHTML = compunds[compuestoRandom].compound.name;
     const representacionCompuesto = document.createElement("th");
-    representacionCompuesto.innerHTML = compunds[compuestoRandom].x.representation;
+    representacionCompuesto.innerHTML = compunds[compuestoRandom].compound.representation;
     newRow.appendChild(nameCompuesto);
     newRow.appendChild(representacionCompuesto);
     table.appendChild(newRow);
